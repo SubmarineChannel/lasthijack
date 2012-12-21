c0485ec090aef88b29e422059a730ae1ecb66cc7
Gijs Kattenberg <gijserman@hotmail.com>
2012-12-21 14:35:02 +0100

    new spalsh screen
    splash screen with loader

--- items/fadein.php
+++ items/fadein.php
@@ -13,28 +13,49 @@
 			});
 		',
 		"content" => '
-			<h1>The Last Hijack explores the current issue of modern day piracy and the lives of the people it touches from both the Western and the Somali perspective.</h1>
 			<center>
 			<table>
-			<tr>
-			<td><img src="images/icons/switch_icon_white.png" /><br />Interact</td>
-			<!--td><img src="images/icons/arrow-up-white.png" /><br />switch perspective</td-->
-			</tr>
+				<tr>
+					<td>
+						<div id="preloader">
+							<div class="progress"></div>
+							<img src="images/icons/skull_loading.png" />
+						</div>
+					</td>
+					<td>
+						<div class="textfield">The Last Hijack explores the current issue of modern day piracy and the lives of the people it touches from both the Western and the Somali perspective.</div>
+					</td>
+				</tr>
+				<tr>
+					<td></td>
+					<td>
+						<div id="logos">
+							<img src="images/logos/sub_logo.png" />
+							<img src="images/logos/ikon_logo.png" />
+							<img src="images/logos/npo_logo.png" />
+							<img src="images/logos/zdf_logo.png" />
+						</div>
+					</td>
+				</tr>
 			</table>
 			</center>
 		',
 		"css" => '
-			#fadein{position:absolute; width:100%; height:100%; background:#000; text-align:center; color:white}
-			#fadein h1{margin-top:15%;}
-			#fadein table{width:400px}
-			#fadein img{height:60px}
+			#fadein{position:absolute; width:100%; height:100%; background:#000; text-align:center;}
+			#fadein table{margin-top:200px; width:1000px}
+			.textfield{font-size:20px; font-weight:bold; background:#fff; padding:20px; margin-left:80px;-moz-border-radius: 16px;-webkit-border-radius: 16px;border-radius: 16px;}
+			#preloader{position:relative; background:#333; width:491px; height:400px; overflow:hidden;}
+			#preloader .progress{background:#fff; position:absolute; width:491px; bottom:0px; left:0px; height:0%; overflow:hidden}
+			#preloader img{height:400px; position:absolute; left:0px; top:0px; }
+			#logos img{margin-top:100px;height:40px;margin-right:45px;}
 		',
 		"javascript" => '
 			function showSplash(){
+				var time = 7000;
 				popcorn.pause();
-				setTimeout(function(){
+				$("#preloader").find(".progress").animate({height:"100%"},time, function(){
 					popcorn.play();
-				}, 5000);
+				});
 			}
 		'
 	);
